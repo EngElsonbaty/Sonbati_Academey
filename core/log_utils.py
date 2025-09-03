f5d7e666cf6f6c28c17ef70c51a6c55035a8c49b
EngElsonbaty <mahmoudelsonbaty43@gmail.com>
2025-09-04 02:06:45 +0300

    Feat: Add logging and timing decorators module

--- core/log_utils.py
+++ core/log_utils.py
@@ -2,3 +2,18 @@ import os
 import sys
 import time
 import logging
+from functools import wraps
+
+def log_and_execute_time_with(func):
+    @wraps
+    def wrappers(*arg, **kwd):
+        pass
+
+    return wrappers
+
+def log_and_execute_time_without(func):
+    @wraps
+    def wrappers():
+        pass
+
+    return wrappers
