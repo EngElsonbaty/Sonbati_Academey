38fba9502b44fb4e6a69ffa6847093062014b884
EngElsonbaty <mahmoudelsonbaty43@gmail.com>
2025-09-07 02:21:29 +0300

    feat: Add generic BaseTemplates class
    This commit introduces the initial version of a reusable base class for table management.
    It aims to reduce code duplication by providing generic methods for CRUD operations.
    
    The class will serve as a template for all future table manager classes.

---
+++ modules/employees/base_tamplates.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+path_modules = os.path.dirname(os.path.dirname(__file__))
+root_path = os.path.dirname(path_modules)
+sys.path.append(path_modules)
+sys.path.append(root_path)
+
+from core.log_utils import log_and_execute_time_with, log_and_execute_time_without
+
+from modules.database_manager import db
+
+
+class BaseTemplates:
+    def __init__(self, table_name: str) -> None:
+        self.table_name = table_name
+
+    @log_and_execute_time_with
+    def create(self, emp_id: int, data: dict):
+        pass
+
+    @log_and_execute_time_with
+    def update(self, emp_id: int, data: dict):
+        pass
+
+    @log_and_execute_time_with
+    def delete(self, emp_id: int):
+        pass
+
+    @log_and_execute_time_with
+    def get(self, emp_id: int):
+        pass
+
+    @log_and_execute_time_with
+    def search(self, full_name: str = "", emp_id: int = 0):
+        pass
